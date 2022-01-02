f278eed27938da92287f54373dd21ace7d738377
adamd <adamd8773@yahoo.fr>
2022-01-02 20:59:08 +0100

    fixed an issue with punctuation that could cause conflict in a csv file

--- passwd_to_file/passwd_to_file.py
+++ passwd_to_file/passwd_to_file.py
@@ -4,6 +4,7 @@ import sys
 
 def passwd(num):
     length = num
+    string.punctuation = string.punctuation.replace(',',';')
     char = string.digits + string.ascii_lowercase + string.ascii_uppercase + string.punctuation
     password = "".join(secrets.choice(char) for i in range(length))
     return password
